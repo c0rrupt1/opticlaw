cd1f611ed75db5c87fe28f19c9f8b7f15c7d0fcc
Rose22 <rosemelody22@gmail.com>
2026-03-03 17:16:05 +0100

    copyright shenanigans

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 #!/bin/env python
 
+# OptiClaw! A modular, token-efficient AI agent framework.
+# Made by Rose22 (https://github.com/Rose22)
+
+# Official github: https://github.com/Rose22/opticlaw
+
+ # This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2.0 of the License, or (at your option) any later version.
+
+ # This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+ # You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
+
 import os
 import sys
 import asyncio
